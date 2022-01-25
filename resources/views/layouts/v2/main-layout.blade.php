68aee2f6d8ad2632c11f78d44264d7a8767546b5
Farkas Richárd <farkas.richard95@gmail.com>
2022-01-25 23:05:08 +0100

    css debug no1: delete asset from head

--- resources/views/layouts/v2/main-layout.blade.php
+++ resources/views/layouts/v2/main-layout.blade.php
@@ -8,8 +8,8 @@
         <!-- <link rel="stylesheet" href="css/style.css"> -->
         <!-- <link href = "{{asset('css/style.css')}}" rel = "stylesheet"> -->
         <!-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous"> -->
-        <link href = "{{asset('css/bootstrap.css')}}" rel = "stylesheet">
-        <link href = "{{asset('css/main.css')}}" rel = "stylesheet">
+        <link href = "/css/bootstrap.css" rel = "stylesheet">
+        <link href = "/css/main.css" rel = "stylesheet">
         @yield('css')
     </head>
     <body>
